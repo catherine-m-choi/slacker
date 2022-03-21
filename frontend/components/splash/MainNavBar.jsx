24cbb232114e000a9d4516ec7d31fd7a5f20fefe
Catherine Choi <catherinemargaretchoi@gmail.com>
2022-03-21 12:58:22 -0400

    Fix Splash billboard button colors

--- frontend/components/splash/MainNavBar.jsx
+++ frontend/components/splash/MainNavBar.jsx
@@ -46,8 +46,8 @@ function MainNavBar(props) {
     </div>
   ) : (
     <div className="MainNavBar__right-menu">
-      <Link to="/signup"><button className="btn purple-solid" >SIGN UP</button></Link>
-      <Link to="/login"><button className="btn purple-transparent" >LOG IN</button></Link>
+      <Link to="/signup"><button className="btn purple-transparent" >SIGN UP</button></Link>
+      <Link to="/login"><button className="btn purple-solid" >LOG IN</button></Link>
     </div>
   )
 
