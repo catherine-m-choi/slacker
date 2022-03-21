bbfe323bf412887f1fc10691661534cea1d3fd3e
Catherine Choi <catherinemargaretchoi@gmail.com>
2022-03-21 16:47:46 -0400

    Add conversations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_21_185503) do
+ActiveRecord::Schema.define(version: 2022_03_21_191549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "conversations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "messages", force: :cascade do |t|
     t.bigint "user_id", null: false
