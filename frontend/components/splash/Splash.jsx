17033fe841175b0c71858131bbc6cc0ef6c661cf
Catherine Choi <catherinemargaretchoi@gmail.com>
2022-03-21 12:44:04 -0400

    Fix splash background positioning

--- frontend/components/splash/Splash.jsx
+++ frontend/components/splash/Splash.jsx
@@ -3,7 +3,7 @@ import { Route } from "react-router-dom";
 import { AuthRoute, ProtectedRoute } from '../../util/route_utils'
 
 import MainNavBarContainer from "./MainNavBarContainer";
-import BillboardV1 from "./BillboardV1";
+// import BillboardV1 from "./BillboardV1";
 import BillboardV2 from "./BillboardV2";
 import BannerCompanies from "./BannerCompanies";
 import MainContent from "./MainContent";
@@ -14,7 +14,6 @@ import SplashFooter from "./Footer";
 
 const Splash = () => (
   <div className="Splash">
-    {/* <h1>[Splash Component]</h1> */}
     <div className="Splash__nav-wrapper">
       <MainNavBarContainer />
       <div className="BillboardV2__background-decoration"></div>
