5d35408e2a91ae78304823ae53e694ac3bb151bd
Catherine Choi <catherinemargaretchoi@gmail.com>
2022-03-27 04:08:34 -0400

    Add styling to profile panel

--- frontend/components/right_sidebar/Profile.jsx
+++ frontend/components/right_sidebar/Profile.jsx
@@ -2,22 +2,63 @@ import React from "react";
 
 function Profile(props) {
   return (
-    <div className="Thread" >
-      <h2>PROFILE</h2>
-      <button onClick={props.closeRightSidebar} >Close me!</button>
-      <img src={props.currentUser.profilePictureUrl} />
-      <ul>
-        <li>{props.currentUser.displayName}</li>
-        <li>{props.currentUser.title}</li>
+    <div className="Profile" >
+      <div className="ChatRoomInfo">
+        <h2>Profile</h2>
+        <button onClick={props.closeRightSidebar} >Close me!</button>
+      </div>
 
-        <button>Edit profile</button>
-        <button>Set status</button>
-        <button>Preferences</button>
-      </ul>
+      <div className="ChatRoom__container">
+        <img src={props.currentUser.profilePictureUrl} />
+          <div className="Profile__name">{props.currentUser.displayName}</div>
+          <div className="Profile__title">{props.currentUser.title}</div>
+          <div className="Profile__status">On vacation</div>
 
-      <br />
-      <br />
-      <div>Local time: 12:23 AM</div>
+        <ul className="Profile__buttons">
+          <li>
+            <span class="material-icons-outlined">sentiment_satisfied_alt</span>
+            <div>Set status</div>
+          </li>
+          <li>
+            <span class="material-icons-outlined">edit</span>
+            <div>Edit profile</div>
+          </li>
+          <li>
+            <span class="material-icons-outlined">settings</span>
+            <div>Preferences</div>
+          </li>
+        </ul>
+
+        <br />
+        <div>Local time: 12:23 AM</div>
+
+        <ul className="Profile__details">
+          <li>
+            <span>Display name</span>
+            <div>catherine.choi</div>
+          </li>
+          <li>
+            <span>Local time</span>
+            <div>3:54 AM3:54 AM</div>
+          </li>
+          <li>
+            <span>Phone number</span>
+            <div>(646) 416-3937</div>
+          </li>
+          <li>
+            <span>Email address</span>
+            <div>catherinemargaretchoi@gmail.com</div>
+          </li>
+          <li>
+            <span>Location</span>
+            <div>New York City</div>
+          </li>
+          <li>
+            <span>Birthday</span>
+            <div>February 23rd, 1994</div>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
